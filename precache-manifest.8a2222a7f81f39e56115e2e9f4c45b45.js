4ffb4de58022d952d02300fdfd0dcb89b48c56a1
Sabahat Iqbal <sabbyiqbal@gmail.com>
2020-03-09 21:56:29 -0400

    Updates

---
+++ precache-manifest.8a2222a7f81f39e56115e2e9f4c45b45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbef8ad77a72547b53163c94a89f8c28",
+    "url": "/SabahatPK/the-urdu-flashcards/index.html"
+  },
+  {
+    "revision": "ef8329ce9cfc40b7ee6a",
+    "url": "/SabahatPK/the-urdu-flashcards/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "2d64c500374991f8c89e",
+    "url": "/SabahatPK/the-urdu-flashcards/static/css/main.ccdda6ad.chunk.css"
+  },
+  {
+    "revision": "ef8329ce9cfc40b7ee6a",
+    "url": "/SabahatPK/the-urdu-flashcards/static/js/2.5705fef5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/SabahatPK/the-urdu-flashcards/static/js/2.5705fef5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d64c500374991f8c89e",
+    "url": "/SabahatPK/the-urdu-flashcards/static/js/main.4e926804.chunk.js"
+  },
+  {
+    "revision": "17fb73473bb30594458e",
+    "url": "/SabahatPK/the-urdu-flashcards/static/js/runtime-main.22a64aa8.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/SabahatPK/the-urdu-flashcards/precache-manifest.8a2222a7f81f39e56115e2e9f4c45b45.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SabahatPK/the-urdu-flashcards/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
